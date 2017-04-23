1378bed0ecd57ca2644485a6747500dbac939961
Kevin Zou <kevinzou2150@gmail.com>
2017-04-22 23:14:08 -0400

    replace tab with 4space

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = function SpamBlock(dispatch){
-	let previous = null;
-	dispatch.hook('S_SYSTEM_MESSAGE', 1, (event) => {
-		if(event.message === previous){
-			return false
-		}
-		if(event.message.length < 6){
-			previous = event.message
-		}
-	});
+    let previous = null;
+    dispatch.hook('S_SYSTEM_MESSAGE', 1, (event) => {
+        if(event.message === previous){
+            return false
+        }
+        if(event.message.length < 6){
+            previous = event.message
+        }
+    });
 }
